559b0679f044ada64582663603be40989c3c3905
alexandregama <alexandre.gama.lima@gmail.com>
2015-07-09 23:25:19 -0300

    Transforma atributos em final pois ItemEstoque é um Value Object

--- src/br/com/casadocodigo/estoque/ItemEstoque.java
+++ src/br/com/casadocodigo/estoque/ItemEstoque.java
@@ -6,9 +6,9 @@ public class ItemEstoque implements Serializable {
 
 	private static final long serialVersionUID = -6468531905839365906L;
 
-	private String codigo;
+	private final String codigo;
 
-	private int quantidade;
+	private final int quantidade;
 
 	public ItemEstoque(String codigo, int quantidade) {
 		this.codigo = codigo;
