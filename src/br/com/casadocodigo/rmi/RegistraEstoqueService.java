bc0ee2b22223804daa3af1b67486f1afbaf280ce
alexandregama <alexandre.gama.lima@gmail.com>
2015-07-10 12:03:27 -0300

    Adiciona mensagem para identificar o inicio e fim do registro da EstoqueService

--- src/br/com/casadocodigo/rmi/RegistraEstoqueService.java
+++ src/br/com/casadocodigo/rmi/RegistraEstoqueService.java
@@ -9,8 +9,12 @@ public class RegistraEstoqueService {
 
 	public static void main(String[] args) {
 		try {
+			System.out.println("Registrando a Estoque Service..");
+			
 			LocateRegistry.createRegistry(1099);
 			Naming.rebind("/estoque", new EstoqueService());
+			
+			System.out.println("Estoque Service finalizada");
 		} catch (RemoteException e) {
 			throw new RuntimeException("Um erro ocorreu na chamada RMI para o servico", e); 
 		} catch (MalformedURLException e) {
